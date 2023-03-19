ca53b574c10f46d15cdc04c55f6590a65819878f
Micaela Gerardi <micaela.gerardi@gmail.com>
2023-03-19 17:16:59 -0300

    añadimos el carrito

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 // array de productos
 
-// const productos = [
-//     {nombre: }
-// ]
+const productos = [
+  { nombre: "chipa", precio: 300 },
+  { nombre: "sandwich de miga", precio: 250 },
+  { nombre: "medialunas de manteca", precio: 150 },
+  { nombre: "cookie con chips", precio: 350 },
+  { nombre: "alfajor de maicena", precio: 200 },
+  { nombre: "pastafrola de batata", precio: 1200 },
+  { nombre: "pastafrola de membrillo", precio: 1200 },
+  { nombre: "berlines rellenos de dulce de leche", precio: 300 },
+  { nombre: "berlines rellenos de pastelera", precio: 300 },
+  { nombre: "budin de banana", precio: 900 },
+  { nombre: "budin de limon", precio: 900 },
+  { nombre: "budin de naranja", precio: 900 },
+  { nombre: "churros", precio: 150 },
+  { nombre: "churros rellenos de dulce de leche", precio: 180 },
+  { nombre: "conitos de dulce de leche", precio: 250 },
+  { nombre: "cookie de chocolate", precio: 350 },
+  { nombre: "pepas", precio: 400 },
+  { nombre: "trufas", precio: 350 },
+  { nombre: "pastelitos rellenos de dulce de leche", precio: 300 },
+  { nombre: "torta fritas", precio: 150 },
+];
+
+// array para carrito
+
+let carrito = [];
